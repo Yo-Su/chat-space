9a0c13102dc48cf7321cc9237235fe5e10bb69ee
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-06 15:56:19 +0900

    create groups_controller_action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,24 @@
 class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def create
+    group.create(group_params)
+    redirect_to root_path
+  end
+
+  def edit
+
+  end
+
+  def update
+    group.update(group_params)
+    redirect_to root_path
+  end
+
+  private
+    def group_params
+      params.require(:group).permit(:name)
+    end
 end
