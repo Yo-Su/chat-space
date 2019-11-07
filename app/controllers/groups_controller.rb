852c49a785c8a255788915b788b7ce9d01bab191
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-07 10:26:01 +0900

    add create_group,validates
    グループの新規作成
    グループ作成バリデーション
    グループ作成フラッシュメッセージ

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,24 +1,40 @@
 class GroupsController < ApplicationController
+  def index
+
+  end
+
   def new
+    # binding.pry
     @group = Group.new
+    @group.users << current_user
+    @users = User.all
+    # binding.pry
   end
 
   def create
-    group.create(group_params)
-    redirect_to root_path
+    @group = Group.new(group_params)
+    @users = User.all
+    # binding.pry
+    if @group.save
+      redirect_to root_path
+      flash[:notice] = "グループを作成しました"
+    else
+      render :new
+    end
   end
 
-  def edit
+  # def edit
+  #   @group = Group.new
+  #   @users = User.all
+  # end
 
-  end
-
-  def update
-    group.update(group_params)
-    redirect_to root_path
-  end
+  # def update
+  #   Group.update(group_params)
+  #   redirect_to root_path
+  # end
 
   private
     def group_params
-      params.require(:group).permit(:name)
+      params.require(:group).permit(:name, user_names: [] )
     end
 end
