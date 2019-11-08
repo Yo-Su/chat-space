dd1e40b25138a64f30d13a3fbb972fb89510ece5
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-08 18:20:41 +0900

    add models-message-test
    +ディレクトリ階層修正

--- db/migrate/20191107060052_create_messages.rb
+++ db/migrate/20191107060052_create_messages.rb
@@ -3,8 +3,8 @@ class CreateMessages < ActiveRecord::Migration[5.0]
     create_table :messages do |t|
       t.text :content
       t.string :image
-      t.integer :user_id, foreign_key: true, null: false
-      t.integer :group_id, foreign_key: true, null: false
+      t.integer :user_id, foreign_key: true
+      t.integer :group_id, foreign_key: true
       t.timestamps
     end
   end
--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :message do
     content {Faker::Lorem.sentence}
     image   {File.open("#{Rails.root}/public/images/test_image.jpg")}
-    user_id
-    group_id
+    user
+    group
   end
 end
+
