606a957a91aaeff11d5e2173c79aaaa45abe6b1c
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-12 17:13:27 +0900

    add done, append 返ってきたJSONをdoneメソッドで受取り、HTMLを作成する 作成したHTMLをメッセージ画面の一番下に追加する

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,34 @@
 $(function(){
+
+  function showImage(message){
+    var image = `<div class="message__text">
+                  <img class="lower-message__image" src="/uploads/message/image/11/nekoneko.jpg" alt="Nekoneko">
+                </div>`
+    return image
+  }
+  function buildMessage(message){
+    // console.log()
+    var image = ""
+    message.image ? image = `<img src="${message.image}">` : image = ""
+    var html = `<div class="message">
+                  <div class="message__upper-info">
+                    <div class="message__upper-info__user">
+                      ${message.user_name}
+                    </div>
+                    <div class="message__upper-info__date">
+                      ${message.time}
+                    </div>
+                  </div>
+                  <div class="message__text">
+                    ${message.content}
+                    <div class="message__text">
+                      ${image}
+                    </div>
+                  </div>
+                </div>`
+    return html
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +40,14 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(message){
+      var html = buildMessage(message);
+      // debugger
+      $('.messages').append(html)
+    })
+    .fail(function(message){
+
     })
   })
 })
