2990d5dc389b9e7b4d9d8322e32729d1ab1b185c
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-13 11:15:23 +0900

    add template-literal, update .done(...) 非同期通信の結果を得て、HTMLを作成 作成したHTMLをビュー上に描画 エラー時の処理追加

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,39 @@
 $(function() {
+
+  var search_list = $("#chat-group-users");
+
+  function searchHit(user){
+    var html = `
+                <div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>
+              `;
+    search_list.append(html);
+  }
+
+  function searchUnHit(){
+    var html = `
+                <div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">ユーザーが見つかりません</p>
+                </div>
+               `;
+    search_list.append(html);
+  }
+
+  function addDeleteUser(name, id){
+    let html = `
+                <div class="ChatMember clearfix" id="${id}">
+                  <p class="ChatMember__name">${name}</p>
+                  <div class="ChatMember__remove ChatMember__button" data-user-id="${id}" data-user-name="${name}">削除</div>
+                </div>
+               `;
+    $("#.ChatMembers").append(html);
+  }
+
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
-    console.log(input);
     $.ajax({
       type: "GET",
       url: "/users",
@@ -9,10 +41,23 @@ $(function() {
       dataType: "json"
     })
     .done(function(users) {
-      console.log(users);
+      search_list.empty();
+
+      if (users.length !== 0){
+        users.forEach(function(user){
+          searchHit(user);
+
+        })
+        console.log(users);
+      } else if (input.length == 0) {
+        return false;
+      }
+      else {
+        searchUnHit();
+      }
     })
     .fail(function() {
-      console.log("失敗です");
+      alert("ユーザー検索に失敗しました");
     });
   });
 });
