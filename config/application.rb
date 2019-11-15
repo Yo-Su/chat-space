f46e0768c183db9ede8d0970de6c45fd82cdef01
Yo-Su <57243991+Yo-Su@users.noreply.github.com>
2019-11-15 17:24:16 +0900

    add application.rb time.zone

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,12 @@ module ChatSpace
     end
   end
 end
+
+module App
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+  end
+end
